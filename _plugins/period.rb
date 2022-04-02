46e207c1cab3a04ef4a85b186fe4db5cce5e9e9b
Castanho Correia <me@castanhocorreia.com>
2022-04-02 16:25:29 -0300

    Fix period filter months bug

--- _plugins/period.rb
+++ _plugins/period.rb
@@ -11,8 +11,8 @@ module Jekyll
 				date_diff = (end_date.year * 12 + end_date.mon) - (start_date.year * 12 + start_date.mon)
 				year_diff = date_diff.divmod(12)[0]
 				mon_diff = date_diff.divmod(12)[1]
-				mon_period = mon_diff < 1 ? "2 mos" : "#{mon_diff + 1} mos"
-				year_period = year_diff <= 0 ? "" : year_diff == 1 ? "#{year_diff} yr" : "#{year_diff} yrs"
+				mon_period = mon_diff == 11 ? "" : mon_diff < 1 && year_diff == 0 ? "2 mos" : "#{mon_diff + 1} mos"
+				year_period = year_diff == 1 || mon_diff == 11 ? "1 yr" : year_diff <= 0 ? "" : "#{year_diff} yrs"
 				return "#{abbr_start_mon} #{start_date.year} – #{end_date == Date.today ? "Present" : "#{abbr_end_mon} #{end_date.year}"} · #{year_period} #{mon_period}".squeeze(" ")
 			end
 		end
