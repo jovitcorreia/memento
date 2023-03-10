048c80b59cb20d4ce68e146a7431c2e20d4ffa55
João Correia <me@castanhocorreia.com>
2023-03-10 16:36:50 -0300

    Add technologies list

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source "https://rubygems.org"
 gem 'jekyll'
 gem 'jekyll-email-protect'
+gem 'jekyll-inline-svg'
 gem 'jekyll-seo-tag'
 gem 'jekyll-sitemap'
 gem 'jekyll-paginate'
+gem 'octokit'
 gem 'webrick'
--- _plugins/period.rb
+++ _plugins/period.rb
@@ -1,3 +1,4 @@
+require 'jekyll'
 require 'date'
 
 module Jekyll
@@ -10,7 +11,7 @@ module Jekyll
         year_diff, mon_diff = date_diff.divmod(12)
         abbr_start_mon = Date::ABBR_MONTHNAMES[start_date.month]
         abbr_end_mon = Date::ABBR_MONTHNAMES[end_date.month]
-        mon_period = mon_diff == 11 ? "" : "#{mon_diff + 1} mos"
+        mon_period = mon_diff == 11 ? "" : mon_diff < 1 && year_diff == 0 ? "1 mo" : "#{mon_diff + 1} mos"
         year_period = year_diff <= 0 ? "" : year_diff == 1 || mon_diff == 11 ? "1 yr" : "#{year_diff} yrs"
         "#{abbr_start_mon} #{start_date.year} – #{end_date == Date.today ? "Present" : "#{abbr_end_mon} #{end_date.year}"} · #{year_period} #{mon_period}".squeeze(" ")
       end
